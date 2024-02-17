2c2a67b74cd68ac9485797b583c68156fa5d0a05
CianciarusoCataldo <cianciaruso.cataldo@gmail.com>
2024-02-17 01:29:27 +0100

    fix

--- docs-gen/utils/generate-docs.sh
+++ docs-gen/utils/generate-docs.sh
@@ -1,16 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-export OUTPUT_ROOT_DIR=docs
-export OUTPUT_STATIC_DOCS_DIR=playground/public/docs
-export DOCS_GEN_DIR=docs-gen
-export COMPONENTS_FILES_DIR=src/components
-export COMPONENTS_DOCS_DIR="$OUTPUT_ROOT_DIR"/components
-export GLOBAL_SETTINGS_DIR=docs-gen/global
-export SHARED_DIR="$OUTPUT_ROOT_DIR"/shared
-export COMPONENTS_SETTINGS_DIR=docs-gen/components
-export TEMPLATE_DIR=docs-gen/templates
-export SCRIPTS_DIR=docs-gen/utils
-export README_CHAPTERS_DIR=docs-gen/templates/readme/chapters
+sh docs-gen/utils/init-env.sh
 
 export PARSE_MODE=global
 
